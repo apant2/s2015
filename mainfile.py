d7fb98e73ef55653e9dc4204db34509c6ec9787d
apant2 <anav.pant@gmail.com>
2015-05-20 13:07:27 -0500

    Added standardized out for the output files

--- mainfile.py
+++ mainfile.py
@@ -2,12 +2,9 @@
 #Output should have same name as input file, say what it does and what is next step.
 #Miscanthus and maize files
 #Comment every function in very basic words
-#Question: Should every file have same name template, or should there be an option to enter unique names for each file?
-
-
 
 #To run script in terminal:
-#  python mainfile.py [-i arg] [-d arg] [-f arg] [-c arg] [-o arg] [-t arg]
+#  python mainfile.py [-i arg] [-d arg] [-c arg] [-o arg] [-t arg]
 
 import pandas as pd
 import numpy as np
@@ -42,7 +39,7 @@ def desired_values(data, depth):
     return(blocks)
 
 #Taking in the blocks, desired filename, and desired output filetype, create a file in the specified directory
-def statistics(blocks, name, iscsv=True):
+def statistics(blocks, name, depth, iscsv=True):
     out_table = pd.DataFrame(columns=['Start','End', 'Std_Dev', 'Avg', 'Median'])
     i=0
     for block in blocks:
@@ -58,17 +55,13 @@ def statistics(blocks, name, iscsv=True):
         out_table.loc[i] = [start_pos, end_pos, std_dev, avg, med]
         i+=1
 
-    new_extension='.csv'
-    if not iscsv:
-        new_extension = '.tsv'
-
     (root, ext) = os.path.splitext(name)
-    os.rename(name, root + new_extension)
+    exit_name = root+str(depth)+".BedCov")
 
     if not iscsv:
-        out_table.to_csv(name+'.tsv', sep='\t')
+        out_table.to_csv(exit_name, sep='\t')
     if iscsv:
-        out_table.to_csv(name+'.csv')
+        out_table.to_csv(exit_name)
 
 #Creates a file for one dataset
 def create_file(datafile, depth, iscsv):
@@ -77,10 +70,10 @@ def create_file(datafile, depth, iscsv):
     statistics(blocks, datafile, iscsv)
 
 #Main function to run the script
-def main(input_directory, depth, iscsv, filetype):
+def main(input_directory, depth, iscsv, fileid):
     files = os.listdir(input_directory)
     for f in files:
-        if(f[-len(filetype):-1]+f[-1] == filetype):
+        if(filetype in f):
             create_file(f, depth, iscsv)
 
 if __name__ == '__main__':
@@ -89,16 +82,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='something')
     parser.add_argument("-i", "--inputdirectory", dest="input", default=os.getcwd(), type=string, help="The input directory of the files")
     parser.add_argument("-d", "--depth", dest="depth", default=5, type=int, help="Minimum depth of window")
-    parser.add_argument("-f", "--filename", dest="filename", default="window_depth", help="name of the output file")
     parser.add_argument("-c", "--iscsv", dest="iscsv", default=True, help="If set to true, the output files will be csv files")
     parser.add_argument("-o", "--outputdirectory", dest="output", default=os.getcwd()+"/output", type=string, help="The output directory of the files")
-    parser.add_argument("-t", "--filetype", dest="filetype", default='.bedCov', type=string, help="The input file extension")
+    parser.add_argument("-t", "--filetype", dest="fileid", default='.bedCov', type=string, help="The input file identifier")
 
     args=parser.parse_args()
-    main(args.input, args.depth, args.iscsv, args.filetype)
+    main(args.input, args.depth, args.iscsv, args.fileid)
 
 
-
-#Don't hardcode anything.
-# Let user choose depth of cutoff
-# name of output file, but set a default for both (5, inputgroup.[depth]).
