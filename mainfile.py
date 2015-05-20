b394ffbab38cf57e0c67a8e41b62f64216def028
apant2 <anav.pant@gmail.com>
2015-05-20 12:32:52 -0500

    Changed variable names, fixed parameters in functions

--- mainfile.py
+++ mainfile.py
@@ -1,62 +1,56 @@
 #To Do:
-#Find and replace var_name 'hey' to something more meaningful
 #Output should have same name as input file, say what it does and what is next step.
 #Miscanthus and maize files
-#Usage- comment out exactly how to execute file in terminal
 #Comment every function in very basic words
-#Don't hardcode anything. Let user choose depth of cutoff, name of output file, but set a default for both (5, inputgroup.[depth]). Look for others as well. Also offer them the choice of outputting as a tab delimited file or csv.
+
+# Look for others as well. Also offer them the choice of outputting as a tab delimited file or csv.
+
+#To run script in terminal:
+#  python mainfile.py [-i arg] [-d arg] [-f arg] [-c arg] [-o arg] [-t arg]
 
 
 import pandas as pd
 import numpy as np
-#import warnings
 import os
-#import sys
-import argparse
 
-#warnings.simplefilter("error")
-
-#To execute the entire script, execute the function whole_script_run() at the end of this script
-
-#Import into pandas datatable and remove all nan values, reset row indices
-def import_clean(datafile):
-    data = pd.read_csv(datafile, sep=" ", header = None, delim_whitespace=True)
-    #data = data.drop([1,2,3,4,5,7,8,10,11], axis=1)
+#Imports and cleans data for easy manipulation and usage.
+def import_data(datafile):
+    data = pd.read_csv(datafile, sep=" ", header=None, delim_whitespace=True)
     data = data[pd.notnull(data[0])]
 
     return(data)
 
-#Creates a 3-D Array. Returns array of arrays, with each sub-array consisting of two arrays: (1) the start and end positions, and
-# (2) the values in there
+#Taking in the data and the depth value, does the following for each block: returns the start position and end position in one array,
+# and the depths for the individual components of the block
 def desired_values(data, depth):
-    hey=[[[],[]]]
+    blocks=[[[],[]]]
     j=0
 
     for i in range(len(data)-1):
         if(data[3].iloc[i]>=depth):
-            hey[j][1].append(data[3].iloc[i])
+            blocks[j][1].append(data[3].iloc[i])
             try:
                 if(data[3].iloc[i-1]<5):
-                    hey[j][0].append(data[1].iloc[i])
+                    blocks[j][0].append(data[1].iloc[i])
             except:
                 pass
             if(data[3].iloc[i+1]<depth):
-                hey[j][0].append(data[2].iloc[i])
-                hey.append([[],[]])
+                blocks[j][0].append(data[2].iloc[i])
+                blocks.append([[],[]])
                 j+=1
-    del hey[-1]
-    return(hey)
+    del blocks[-1]
+    return(blocks)
 
-#Return std. dev,
-def statistics(hey, datafile, iscsv):
+#Taking in the blocks, desired filename, and desired output filetype, create a file in the specified directory
+def statistics(blocks, name, iscsv=True):
     out_table = pd.DataFrame(columns=['Start','End', 'Std_Dev', 'Avg', 'Median'])
     i=0
-    for elem in hey:
+    for block in blocks:
         vals=[]
-        vals_np = np.array(elem[1])
+        vals_np = np.array(block[1])
 
-        start_pos = elem[0][0]
-        end_pos = elem[0][1]
+        start_pos = block[0][0]
+        end_pos =block[0][1]
         std_dev = np.std(vals_np)
         avg = np.average(vals_np)
         med = np.median(vals_np)
@@ -66,32 +60,40 @@ def statistics(hey, datafile, iscsv):
 
     name = datafile.replace(".bedCov", "outputval.csv")
 
-    if not csv:
+    if not iscsv:
         out_table.to_csv(name, sep='\t')
-    if csv:
+    if iscsv:
         out_table.to_csv(name)
 
 #Function to run for one file
-def wrapper_fn(datafile, depth, iscsv):
-    data = import_clean(datafile)
-    hey = desired_values(data, depth)
-    statistics(hey, datafile, iscsv)
+def create_file(datafile, depth, iscsv):
+    data = import_data(datafile)
+    blocks = desired_values(data, depth)
+    statistics(blocks, datafile, iscsv)
 
 #Main function to run the script
-def main(depth, iscsv):
-    files = os.listdir(os.getcwd())
+def main(input_directory, depth, iscsv, filetype):
+    files = os.listdir(input_directory)
     for f in files:
-        if(f[-6:-1]+f[-1] == 'bedCov'):
-            wrapper_fn(f, depth, iscsv)
-
+        if(f[-len(filetype):-1]+f[-1] == filetype):
+            create_file(f, depth, iscsv)
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='something')
+    parser.add_argument("-i", "--inputdirectory", dest="input", default=os.getcwd(), type=string, help="The input directory of the files")
     parser.add_argument("-d", "--depth", dest="depth", default=5, type=int, help="Minimum depth of window")
     parser.add_argument("-f", "--filename", dest="filename", default="window_depth", help="name of the output file")
-    parser.add_argument("-c", "--iscsv", dest="iscsv", default=TRUE, help="If set to true, the output files will be csv files")
+    parser.add_argument("-c", "--iscsv", dest="iscsv", default=True, help="If set to true, the output files will be csv files")
+    parser.add_argument("-o", "--outputdirectory", dest="output", default=os.getcwd()+"/output", type=string, help="The output directory of the files")
+    parser.add_argument("-t", "--filetype", dest="filetype", default='.bedCov', type=string, help="The input file extension")
 
     args=parser.parse_args()
-    main(args.depth, args.iscsv)
+    main(args.input, args.depth, args.iscsv, args.filetype)
+
+
+
+#Don't hardcode anything.
+# Let user choose depth of cutoff
+# name of output file, but set a default for both (5, inputgroup.[depth]).
