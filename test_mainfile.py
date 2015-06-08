acc80d50e6c2a0a40e569a90e129034713da0d4f
apant2 <anav.pant@gmail.com>
2015-06-08 10:59:12 -0500

    Added a new example with it's output at start of file; expanded on the function descriptions

--- test_mainfile.py
+++ test_mainfile.py
@@ -1,34 +1,69 @@
 #!/usr/local/bin/python
 #/usr/lib/python2.7
 
-#To Do:
-#Miscanthus and maize files
-#Comment every function in very basic words
-
 #For help on how to run this script in bash, run the following:
 #  python mainfile.py --help
+
+#For proper output, all input files must be in the format as the block of text below:
+#
+#Scaffold810	132577	133699	0
+#Scaffold810	133699	133703	4
+#Scaffold810	133703	133706	7
+#Scaffold810	133706	133707	8
+#Scaffold810	133707	133709	10
+#Scaffold810	133709	133710	16
+#Scaffold810	133710	133714	17
+#Scaffold810	133714	133723	18
+#Scaffold810	133723	133724	16
+#Scaffold810	133724	133725	13
+#Scaffold810	133725	133730	12
+#Scaffold810	133730	133731	11
+#Scaffold810	133731	133734	3
+#Scaffold810	133734	133736	2
+#Scaffold810	133736	133738	1
+#Scaffold810	133738	133753	11
+#Scaffold810	133753	133761	10
+#Scaffold810	133761	133798	0
+#Scaffold810	133798	133819	1
+#Scaffold810	133819	133969	0
+#Scaffold810	133969	133990	1
+#Scaffold810	133990	134039	0
+#Scaffold810	134039	134043	1
+#Scaffold810	134043	134044	2
+#Scaffold810	134044	134046	3
+#Scaffold810	134046	134047	7
+#Scaffold810	134047	134054	10
+#Scaffold810	134054	134061	11
+#Scaffold810	134061	134068	10
+
+#When this script is run from terminal with default values on the above data, the following is what the output file will contain:
+#Scaffold,Start,End,Window Size,Std_Dev,Avg,Median
+#Scaffold810,133703,133731,28,3.6551333764994136,12.8,12.5
+#Scaffold810,133738,133761,23,0.5,10.5,10.5
+#Scaffold810,134046,134068,22,1.5,9.5,10.0
+
 
 import csv
 import os
 
-#Cleans and returns data for easy usage by this script
+#Cleans and returns data in the above format for easy usage by this script
 def import_data(datafile):
-    array=[]
+    data=[]
     with open(datafile, 'r') as f:
         for line in f:
             words = line.split()
             if words:
-                array.append(words)
-    return array
+                data.append(words)
+    return data
 
 
-#Taking in the cleaned data and the depth value, does the following for each block that it finds: returns the start position, end position,
-# and all the depths for the individual components of the block
+# Taking in the cleaned data (data) and the depth value (depth), does the following for every block of rows with depth greater than or equal to depth:
+# Returns the start position, end position, and all the depths for the individual components of the block in an array
 def desired_values(data, depth):
     blocks=[[[],[]]]
     j=0
     index = 0
-    
+
     for row in data:
         if row:
             if(int(row[3])>=depth):
@@ -52,6 +87,7 @@ def desired_values(data, depth):
 
     return(blocks)
 
+#Taking an array or list of values, returns the average of the values
 def average(values):
     sum=0
     for elem in values:
@@ -59,6 +95,7 @@ def average(values):
     
     return(sum/len(values))
 
+#Taking an array or list of values, returns the standard deviation of the values
 def std_dev(values):
     var=0
     avg=average(values)
@@ -68,6 +105,7 @@ def std_dev(values):
     
     return stdev
 
+#Taking an array or list of values, returns the median of the values
 def median(numericValues):
     theValues = sorted(numericValues)
     if len(theValues) % 2 == 1:
@@ -78,11 +116,14 @@ def median(numericValues):
         return (float(lower + upper)) / 2
 
 
-#Taking in the blocks, the name of the file being analyzed, the is CSV option, and the desired output filetype, this function creates an output file in the specified directory.
+#Function input: array returned from desired_values, the name of the file being analyzed, the is CSV option, and the desired output directory
+#What is returned by the function: A delimitered file with each row having the following information-
+#Name of Scaffold, Start Position , End Position,Window Size (Difference of End and Start Position), Standard Deviation of the depths in each block, Average of the depths, Median of the depths
+
 def statistics(blocks, name, depth, iscsv, outputdirectory):
     
     (root, ext) = os.path.splitext(name)
-    exit_name = outputdirectory+root+str(depth)+".bedCov"
+    exit_name = outputdirectory+root+".depth"+str(depth)+".bedCov"
     
     #If the desired output directory does not exist, it is created
     if not os.path.exists(outputdirectory):
@@ -103,55 +144,46 @@ def statistics(blocks, name, depth, iscsv, outputdirectory):
         med = median(values)
         out.append((scaffold,start_pos,end_pos,window_size,stddev,avg,med))
     
-    if iscsv:
-        d=","
-    elif not iscsv:
+    d=","
+    if not iscsv:
         d="\t"
     
-    if iscsv:
-        f=open(exit_name, 'wt')
-        try:
-            writer = csv.writer(f, delimiter=d)
-            writer.writerow( ('Scaffold','Start','End','Window Size','Std_Dev', 'Avg', 'Median') )
-            for row in out:
-                writer.writerow( (row[0], row[1], row[2], row[3],row[4],row[5],row[6]) )
-        finally:
-            f.close()
-
-
-#
-#
-#    if not iscsv:
-#        f=open(exit_name, 'wt')
-#        try:
-#            writer = csv.writer(f, delimiter="\t")
-#            writer.writerow( ('Start','End', 'Std_Dev', 'Avg', 'Median') )
-#            for row in out:
-#                writer.writerow( (row[0], row[1], row[2], row[3],row[4],row[5],row[6]) )
-#        finally:
-#            f.close()
+    f=open(exit_name, 'wt')
+    try:
+        writer = csv.writer(f, delimiter=d)
+        writer.writerow( ('Scaffold','Start','End','Window Size','Std_Dev', 'Avg', 'Median') )
+        for row in out:
+            writer.writerow( (row[0], row[1], row[2], row[3],row[4],row[5],row[6]) )
+    finally:
+        f.close()
 
 #Creates a file for one dataset
+#Function input: An input file in the above format, the cutoff value of the depth, whether the file is a csv (true) or tsv (false), and the output file's directory
+#Function output: An output file
 def create_file(datafile, depth, iscsv, outputdirectory):
     data = import_data(datafile)
     blocks = desired_values(data, depth)
     statistics(blocks, datafile, depth, iscsv, outputdirectory)
 
-#converts some string version of the word "true" to its boolean expression; if the string inputted is not a form of 'true', returns false. Used in another function.
+#Converts a string to a boolean value
+#Function input: A string
+#Function output: If it is some version of the word "true", returns boolean value True. Else, returns boolean value false
 def isTrue(bool_statement):
     if bool_statement.lower()=='true':
         return True
     else:
         return False
 
-#Main function to run the script
+#The main function of the script
+#Function input: A directory that contains files with the format shown at the start of this script, the cutoff value of the depth, whether the file is a csv (true) or tsv (false), the slice of text which identifies the files we want to run through this script, and the directory of the output files
+#Function output: In the directory outputdirectory, creates output files for each input file
 def main(input_directory, depth, iscsv, fileid, outputdirectory):
     files = os.listdir(input_directory)
     for f in files:
         if(fileid in f):
             create_file(f, depth, iscsv, outputdirectory)
 
-#The below code will run only when this file is executed as the main file. When this file is imported as a module the code below will not execute.
+#The below code will run only when this file is executed as the main file. When this file is imported as a module into another the code below will not execute.
 if __name__ == '__main__':
     import argparse
     import time
